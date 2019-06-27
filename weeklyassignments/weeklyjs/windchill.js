ead16e5235c0295b8bacd3d9f6067f316df2be36
brie2014 <brie2014@byui.edu>
2019-06-27 02:35:46 -0400

    done

--- weeklyassignments/weeklyjs/windchill.js
+++ weeklyassignments/weeklyjs/windchill.js
@@ -33,11 +33,10 @@ function doInputOutput() {
 
 function windChill(tempF, speed) {
 	if (tempF <= 50 && speed >= 3) {
-		// f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16
 		let wc = 35.74 + 0.6215*tempF - 35.75*Math.pow(speed, 0.16) + 0.4275*tempF*Math.pow(speed,0.16)
 		return wc.toFixed(2) + "&deg;" + "F"; 
 	}
 	else {
 	  return "N/A";
-	}
+		}
 }
