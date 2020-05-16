82c21793e335022a167f3c71dc9a2062bbcd73c9
brie2014 <brie2014@byui.edu>
2020-05-16 11:33:27 -0400

    final week 4

--- WDD230/lesson4/js/preston.js
+++ WDD230/lesson4/js/preston.js
@@ -1,11 +1,13 @@
 //Menu button toggle
 const hambutton=document.querySelector('.ham');
-const mainnav=document.querySelector('.navigation');
+const mainnav=document.querySelector('.responsive');
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')});
 
 
-//Date
+//Date in this format Wednesday, 24 July 2020
 let today = new Date();
-let date= today.getMonth()+1 + '/' + today.getDate() + '/' + today.getFullYear();;
+let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let date = weekdays[today.getDay()] + ', ' + today.getDate() + " " + months[today.getMonth()] + " " + today.getFullYear();
 document.getElementById('currentdate').innerHTML=date;
