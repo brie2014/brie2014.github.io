bfa64d8a87e0c0b586df63b9eea42b06cf2a2068
brie2014 <brie2014@byui.edu>
2020-05-30 16:49:12 -0400

    fixed windchill

--- WDD230/lesson6/js/windchill.js
+++ WDD230/lesson6/js/windchill.js
@@ -1,6 +1,6 @@
 //Caluculate Windchill using elements from a page
-let t = document.getElementById('windspeed').value;
-let s = document.getElementById('currentTemp').value;
+let t = document.getElementById('currentTemp').textContent;
+let s = document.getElementById('windspeed').textContent;
 let w = windChill(t, s);
 document.getElementById('windchill').textContent = w;
 
@@ -8,7 +8,7 @@ document.getElementById('windchill').textContent = w;
 function windChill(tempF, speed) {
 	if (tempF <= 50 && speed >= 3) {
         let wc = 35.74 + 0.6215*tempF - 35.75*Math.pow(speed, 0.16) + 0.4275*tempF*Math.pow(speed,0.16)
-        return wc.toFixed(2) + "&deg;" + "F"; 
+        return wc.toFixed(1); 
     }
 	else {
 	  return "N/A";
