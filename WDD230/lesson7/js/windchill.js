e0b24816a9ef8b41b3c33a2d1590c6fbf3edeba7
brie2014 <brie2014@byui.edu>
2020-06-03 17:39:23 -0400

    lazy load assignment done

--- WDD230/lesson7/js/windchill.js
+++ WDD230/lesson7/js/windchill.js
@@ -1,18 +1,15 @@
 //Caluculate Windchill using elements from a page
-let t = 45;
-let s = document.getElementById('currentTemp').val;
-let windchill = windChill(t, s); 
-document.getElementById('windchill').textContent = windchill;
+let t = document.getElementById('currentTemp').textContent;
+let s = document.getElementById('windspeed').textContent;
+document.getElementById('windchill').textContent = windChill(t, s);
 
 
 function windChill(tempF, speed) {
 	if (tempF <= 50 && speed >= 3) {
         let wc = 35.74 + 0.6215*tempF - 35.75*Math.pow(speed, 0.16) + 0.4275*tempF*Math.pow(speed,0.16)
-        return wc.toFixed(2) + "&deg;" + "F"; 
+        return wc.toFixed(1); 
     }
 	else {
 	  return "N/A";
 		}
 }
-
-//document.getElementById('windspeed').value;
