36b27ab5b640163cb5c3ad4f7a0dbc8b257ee915
brie2014 <brie2014@byui.edu>
2020-06-11 14:06:04 -0400

    table build updated

--- WDD230/lesson8/js/basic-footer.js
+++ WDD230/lesson8/js/basic-footer.js
@@ -1,3 +1,12 @@
 let today = new Date();
 let date = today.getFullYear();
 document.getElementById('currentdate').innerHTML = date;
+
+//Google Fonts API Loader
+WebFont.load({
+    google: {
+      families: [
+         'Ubuntu'
+      ]
+    }
+  })
